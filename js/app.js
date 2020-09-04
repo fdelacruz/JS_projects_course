deca115a4bf483df91908b12e15478801972b53c
Francisco De La Cruz <helloquico@gmail.com>
2020-09-04 11:08:57 -0400

    String Properties and Methods

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
-let i;
-for (i = 0; i < 10; i++) {
-  console.log('and the number is: ' + i);
-}
+// String properties and methods
+
+let text = ' Peter Jordan';
+let result = text.length;
+console.log(result);
+
+console.log(text.length);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.charAt(0));
+console.log(text.charAt(text.length - 1));
+console.log(text.indexOf('P'));
+console.log(text.indexOf('n'));
+console.log(text);
+console.log(text.trim());
+console.log(text.trim().toLowerCase().startsWith('peter'));
+console.log(text.includes('eter'));
+console.log(text.slice(0, 2));
+console.log(text.slice(-3));
+
+// const person = {
+//   name: 'peter',
+//   greeting() {
+//     console.log("Hey, I'm Peter");
+//   },
+// };
+
+// console.log(person);
+
+// console.log(person.name);
+// person.greeting();
