844468336892faaa3694c1373091f97a05ab2b45
Francisco De La Cruz <helloquico@gmail.com>
2020-09-07 13:21:21 -0400

    Array Iterators - find

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
-// filter
-// does return a new array
-// can manipulate the size of the new array
-// returs based on condition
+// find
+// returns single instance, in this case, an object
+// returns first match, if no match then undefined
+// great for getting the unique value
 
 const people = [
-  {name: 'susy', age: 20, position: 'developer'},
-  {name: 'peter', age: 25, position: 'designer'},
-  {name: 'john', age: 35, position: 'devops'},
+  {name: 'susy', age: 20, position: 'developer', id: 1},
+  {name: 'peter', age: 25, position: 'designer', id: 2},
+  {name: 'john', age: 35, position: 'devops', id: 3},
 ];
 
-const youngPeople = people.filter((person) => {
-  return person.age <= 25
+// const names = ['bob', 'peter', 'susy'];
+// console.log(
+//   names.find((name) => {
+//     return name === 'bob'
+//   }));
+
+const person = people.find((person) => {
+  return person.id === 3
 });
 
-const developer = people.filter((person) => {
-  return person.position == 'developer'
+console.log(person.name);
+
+const person2 = people.filter((person) => {
+  return person.id === 3
 });
 
-console.log(youngPeople);
+console.log(person2[0].name);
