2c3260254669acd9e16f10999ec2ab0f1af29fad
Francisco De La Cruz <helloquico@gmail.com>
2020-09-08 10:09:35 -0400

    Array Iterators - Date Object

--- js/app.js
+++ js/app.js
@@ -1,19 +1,42 @@
-// Math
-// Standard built-in object - always available
+// Date
 
-// const number = 4.56789
-// const result = Math.floor(number);
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
-// const number = 4.12222;
-// const result = Math.ceil(number);
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
-// const number = 25
-// const result = Math.sqrt(number);;
+// const date = new Date;
+const date = new Date('1/27/1971');
+const month = date.getMonth();
+// console.log(months[month]);
 
- // const result = Math.PI;
- // const result = Math.min(4, 5, 6, 7, 9);
- // const result = Math.max(4, 5, 6, 7, 9);
+const day = date.getDay();
+// console.log(days[day]);
 
- const result = Math.floor(Math.random() * 10 + 1);
+// console.log(date.getDate());
+// console.log(date.getFullYear());
 
-console.log(result);
+const sentence = `${days[day]}, ${date.getDate()} ${months[month]} ${date.getFullYear()}`;
+
+// console.log(sentence);
+document.body.innerHTML = sentence;
