1c50a55c370784add89c822022547ce9d4be0160
Francisco De La Cruz <helloquico@gmail.com>
2020-09-13 22:55:12 -0400

    Get Elements By Class Name

--- js/app.js
+++ js/app.js
@@ -1,28 +1,10 @@
 // Select the element or group of elements that we want
 // Decide the effect we want to apply to the  selection
 
-// getElementsByTagName('tagname');
+// getElementsByClassName('classname');
 
 // node-list = array-like object
 // index, length property but not array methods
 
-const headings = document.getElementsByTagName('h2');
-headings[0].style.color = 'red';
-// console.log(headings.length);
-
-const items = document.getElementsByTagName('li');
-// items.forEach((item) => {
-//   console.log(item);
-// });
-
-items[2].style.color = 'orange';
-
-const listItems = [...items];
-
-listItems.forEach((item) => {
-  console.log(item);
-});
-
-console.log(items);
-console.log(listItems);
-
+const listItems = document.getElementsByClassName('special');
+listItems[1].style.color = 'blue';
