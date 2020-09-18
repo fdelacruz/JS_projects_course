966e39841c308a573fb6cf8404500efb1dfbfa30
Francisco De La Cruz <helloquico@gmail.com>
2020-09-18 09:17:09 -0400

    Navigate the DOM - parentElement

--- js/app.js
+++ js/app.js
@@ -1,18 +1,5 @@
-// Select the element or group of elements that we want
-// Decide the effect we want to apply to the  selection
+// parentElement
 
-// childNodes = return all childNodes including whitespace which is treated as a text node
-
-// children
-// firstChild
-// lastChild
-
-const result = document.querySelector('#result');
-const allChildren = result.childNodes;
-// console.log(allChildren);
-
-// console.log(result.children);
-console.log(result.children);
-
-console.log(result.firstChild);   // whitespace
-console.log(result.lastChild);
+const heading = document.querySelector('h2');
+const parent = heading.parentElement;
+parent.style.color = 'red'
