1ee7a0c2ce68cd633cb1eaf4cae874fc8378347b
Francisco De La Cruz <helloquico@gmail.com>
2020-09-19 09:08:13 -0400

    getAttribute() setAttribute()

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
-// nodeValue
-// textContent
+// getAttribute()
+// setAttribute()
 
-const item = document.getElementById('special');
-const value = item.firstChild.nodeValue;
-const easyValue = item.textContent;
+const first = document.querySelector('.first');
+const idValue = first.getAttribute('id')
+// console.log(idValue);
 
-console.log(easyValue);
+const link = document.getElementById('link');
+const showLink = link.getAttribute('href');
+console.log(showLink);
+
+const last = link.nextElementSibling;
+last.setAttribute('class', 'first')
+last.textContent = 'i also have a class of first'
+console.log(last);
+
+
+const links = document.querySelectorAll('.first');
+console.log(links);
