25b18be1c23a97216e1f54161be21d5045128e51
Francisco De La Cruz <helloquico@gmail.com>
2020-09-20 22:08:18 -0400

    Function Reference

--- js/app.js
+++ js/app.js
@@ -5,6 +5,13 @@
 const btn = document.querySelector('.btn');
 const heading = document.querySelector('h2');
 
-btn.addEventListener('click', () => {
-  heading.classList.add('red')
-});
+function changeColor() {
+  let hasClass = heading.classList.contains('red');
+  if (hasClass) {
+    heading.classList.remove('red')
+  } else {
+    heading.classList.add('red')
+  }
+}
+
+btn.addEventListener('click', changeColor);
