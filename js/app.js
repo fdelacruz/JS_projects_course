32b82b339be78086ae894f9cbbb031b67fcd6f89
Francisco De La Cruz <helloquico@gmail.com>
2020-09-21 21:24:53 -0400

    Event Propagation - Bubbling - Capturing

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
-// currentTarget - always refers to the element to which the event handler
-// had been attached to
-// target - identifies the element on which the event occured
+// allows select dynamic element
+// event propagation - order the events are fired
+// event bubbling - clicked element first the bubbles up -- default
+// event capturing - fires at the root and fires until reaches target
 
-   
-const btns = document.querySelectorAll('.btn');
-btns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    // console.log(e.currentTarget);
-    // e.currentTarget.style.background = 'black';
-    console.log('target', e.target);
-    e.target.style.background = 'black'
-  });
-});
+const container = document.querySelector('.container');
+const list = document.querySelector('.list-items');
+
+function showBubbling(e) {
+  console.log('current target', e.currentTarget);
+  console.log('target', e.target);
+  if (e.target.classList.contains('link')) {
+    console.log('link clicked');
+  }
+}
+
+function stopPropagation(e) {
+  console.log('clicked');
+  e.stopPropagation();
+}
+
+list.addEventListener('click', showBubbling, {capture: true})
+container.addEventListener('click', showBubbling, {capture: true})
+document.addEventListener('click', showBubbling, {capture: true})
+window.addEventListener('click', showBubbling, {capture: true})
