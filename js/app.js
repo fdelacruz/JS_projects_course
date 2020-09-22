905a8dc17b671025934e5ba9410fd6f89e93cbfb
Francisco De La Cruz <helloquico@gmail.com>
2020-09-22 17:03:50 -0400

    Local Storage With Multiple Values

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
-// Web Storage API - provided by the browser
-// session storage , local storage
-// setItem, getItem, removeItem, clear
+// JSON.stringify(), JSON.parse()
 
-localStorage.setItem('name', 'francisco');
-localStorage.setItem('lastName', 'de la cruz');
-localStorage.setItem('ss', '555-55-5555')
-// sessionStorage.setItem('name', 'francisco');
+const friends = ['john', 'peter', 'robert'];
 
-const name = localStorage.getItem('name');
-console.log(name);
-localStorage.removeItem('ss')
-localStorage.clear()
+localStorage.setItem('friends', JSON.stringify(friends))
+
+const values = JSON.parse(localStorage.getItem('friends'));
+// console.log(values[0]);
+
+let fruits;
+
+if (localStorage.getItem('fruits')) {
+  fruits = JSON.parse(localStorage.getItem('fruits'));
+} else {
+  fruits = [];
+}
+
+console.log(fruits);
+// fruits.push('apple');
+fruits.push('banana');
+localStorage.setItem('fruits', JSON.stringify(fruits));
