6c80cdc39036399aef81460c3ec18dbfd1312c08
Francisco De La Cruz <helloquico@gmail.com>
2021-04-20 06:45:21 -0400

    'this' - keyword basics

---
+++ this_keyword_basics/app.js
@@ -0,0 +1,13 @@
+// this
+// points to the left of the dot
+
+const john = {
+  firstName: 'john',
+  lastName: 'anderson',
+  fullName: function () {
+    console.log(this)
+    console.log(`My full name is ${this.firstName} ${this.lastName}`)
+  }
+}
+
+john.fullName()
