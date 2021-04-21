daa4ca0205fd8e9b2af6b2e56c21e6fb6bd91dd5
Francisco De La Cruz <helloquico@gmail.com>
2021-04-20 23:22:46 -0400

    Bind

--- call-apply-bind/app.js
+++ call-apply-bind/app.js
@@ -1,5 +1,6 @@
 // call - runs instantly, arguments - list of items
 // apply - runs instantly, arguments - array of items
+// bind - assign, user later, arguments - list of items
 
 const john = {
   name: 'john',
@@ -16,10 +17,6 @@ function greet(city, country) {
   console.log(`Hello, I'm ${this.name}, and I'm ${this.age} years old and I live in ${city}, ${country}`);
 }
 
-// greet.call(john, 'san diego', 'us')
-// greet.call(susan, 'san diego', 'us')
-// greet.call({name: 'peter', age: 45}, 'san diego', 'us')
-
-greet.apply(john, ['san diego', 'us']);
-greet.apply(susan, ['san diego', 'us']);
-greet.apply({ name: 'peter', age: 30 },['san diego', 'us']);
+// assign, call it later
+const susanGreet  = greet.bind(susan, 'toronto', 'ca');
+susanGreet()
