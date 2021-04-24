6cf86ab176ee0cec0160c6b903af2417b8541d8c
Francisco De La Cruz <helloquico@gmail.com>
2021-04-24 00:35:38 -0400

    Arrow Functions - Select Elements and "this"

--- ES6/app.js
+++ ES6/app.js
@@ -1,25 +1,12 @@
 // Arrow Functions or Fat Arrow Functions
 // reg function: 'this' refers parent, left of the dot
 // arrow function: refers to it's current surrounding scope
-const bob = {
-  firstName: 'bob',
-  lastName: 'smith',
-  sayName: function () {
-    console.log(this)
-    setTimeout(() => {
-      console.log(`Hello, my name is ${this.firstName} ${this.lastName}`)
-    }, 2000)
-  }
-}
 
-const anna = {
-  firstName: 'anna',
-  lastName: 'sanders',
-  sayName: () => {
-    console.log(this)
-    console.log(`Hello, my name is ${this.firstName} ${this.lastName}`)
-  }
-}
+const btn = document.querySelector('.btn')
 
-bob.sayName()
-anna.sayName()
+btn.addEventListener('click', function () {
+  console.log(this)
+  setTimeout(() => {
+    this.style.color = 'black'
+  }, 3000)
+})
