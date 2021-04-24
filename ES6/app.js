8dc73d87f1bcbd06206de412505627bfb4675148
Francisco De La Cruz <helloquico@gmail.com>
2021-04-24 00:04:32 -0400

    Arrow Functions - Objects and "this"

--- ES6/app.js
+++ ES6/app.js
@@ -1,46 +1,25 @@
 // Arrow Functions or Fat Arrow Functions
-// no name, always expressions, assign to variable
-// no function keyword
-
-// function sayHi() {
-//   console.log('hello');
-// }
-// sayHi()
-
-// const hello = function(name) {
-//   console.log(`Hello ${name}`);
-// };
-// hello('bob')
-
-// function triple(val1, val2) {
-//   return val1 * val2 * 3
-// }
-// triple()
-
-const sayHi = () => console.log('hello there')
-sayHi()
-
-const double = value => value * 2
-console.log(double(4))
-
-const multiply = (num1, num2) => {
-  const result = num1 * num2
-  return result
+// reg function: 'this' refers parent, left of the dot
+// arrow function: refers to it's current surrounding scope
+const bob = {
+  firstName: 'bob',
+  lastName: 'smith',
+  sayName: function () {
+    console.log(this)
+    setTimeout(() => {
+      console.log(`Hello, my name is ${this.firstName} ${this.lastName}`)
+    }, 2000)
+  }
 }
-const result = multiply(4, 6)
-console.log(result)
 
-// return object
-const object = () => ({ name: 'john', age: 50 })
-const person = object()
-console.log(person)
+const anna = {
+  firstName: 'anna',
+  lastName: 'sanders',
+  sayName: () => {
+    console.log(this)
+    console.log(`Hello, my name is ${this.firstName} ${this.lastName}`)
+  }
+}
 
-// arrow functions as callbacks
-const numbers = [1, 2, 3, 4, 5, 6]
-const odds = numbers.filter(n => {
-  return n % 2 === 0
-})
-console.log(odds)
-
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', () => console.log('You clicked me!!'))
+bob.sayName()
+anna.sayName()
