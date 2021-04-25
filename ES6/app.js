8ed29e63036edbd961670e56c5d30bc584502836
Francisco De La Cruz <helloquico@gmail.com>
2021-04-24 22:26:40 -0400

    For of - Loop

--- ES6/app.js
+++ ES6/app.js
@@ -1,22 +1,25 @@
-// new string methods
-// starsWith(), endsWith(), includes(), repeat()
+// for of - loops through the values of an iterable object
+// String, Array, Map, Set, etc - NOT Objects
+// unlike forEach - we can use break, continue
 
-const person = 'Peter Smith'
-const employee = '23456-EMP-PETER-SMITH'
-const manager = '23456-MAN-JOHN-DOE'
+const fruits = ['apple', 'orange', 'banana', 'peach']
+const longName = 'John Smith Peter II'
+let shortName = ''
 
-// startsWith
-console.log(person.startsWith('Pet'))
-console.log(employee.startsWith('EMP', 6))
+for (const letter of longName) {
+  // console.log(letter);
+  if (letter === ' ') {
+    continue
+  } else {
+    shortName += letter
+  }
+}
+console.log(shortName)
 
-// endsWith
-console.log(manager.endsWith('DOE'))
-console.log(manager.endsWith('MAN', 9))
-
-// includes
-console.log(employee.includes('PETER'))
-
-// repeat
-const multiplyPeople = (person, amount = 5) => person.repeat(amount)
-const people = multiplyPeople(person)
-console.log(people)
+for (const fruit of fruits) {
+  if (fruit === 'banana') {
+    // break;
+    continue
+  }
+  console.log(fruit)
+}
