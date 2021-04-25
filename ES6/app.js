17d32ff2ea5319238b2bb9bc5d44b327a49b63dc
Francisco De La Cruz <helloquico@gmail.com>
2021-04-24 22:44:55 -0400

    Spread Operator - Objects

--- ES6/app.js
+++ ES6/app.js
@@ -1,19 +1,8 @@
 // Spread Operator ...
 // Allows an iterable to spread/expand individually inside receiver
 // Split  into single items and copy them
+// ES2018 - ES8
 
-const udemy = 'udemy';
-const letters = [...udemy];
-// console.log(letters);
-
-const boys = ['john', 'peter', 'bob'];
-const girls = ['susan', 'anna'];
-const bestFriend = 'arnold';
-
-const friends = [...boys, ...girls, bestFriend];
-// console.log(friends);
-
-const newFriends = [...friends];
-newFriends[0] = 'karina'
-console.log(friends);
-console.log(newFriends);
+const person = { name: 'john', job: 'developer' }
+const newPerson = { ...person, age: 50, name: 'francisco' }
+console.log(newPerson)
