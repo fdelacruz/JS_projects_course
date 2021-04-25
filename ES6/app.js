5a8275343d968a366fef60f5aff427f186803386
Francisco De La Cruz <helloquico@gmail.com>
2021-04-25 17:25:21 -0400

    Array.of

--- ES6/app.js
+++ ES6/app.js
@@ -1,26 +1,13 @@
-// Rest Operator ...
-// gathers/collects the items
+// Array.from and Array.of - NOT ON THE PROTOTYPE
+// Array.from
+// Array.of
+// of creates a new Array instance from a variable number of arguments
 
-// arrays
-const fruits = ['apple', 'orange', 'lemon', 'banana', 'pear']
-const [first, second, ...fruit] = fruits
-// console.log(first, fruit)
+const example = ['one', 'two', 'three']
+// console.log(example)
+// console.log(example.map)
+// console.log(example.from)
+// console.log(example.of)
 
-// objects
-const person = { name: 'john', lastName: 'smith', job: 'developer' }
-const { job, ...rest } = person
-// console.log(job, rest)
-
-const testScores = [78, 90, 56, 43, 99, 65]
-
-const getAverage = (name, ...scores) => {
-  console.log(name)
-  console.log(scores)
-  let total = 0
-  for (const score of scores) {
-    total += score
-  }
-  console.log(`${name}'s average score is ${total / scores.length}`)
-}
-
-getAverage(person.name, ...testScores)
+const friends = Array.of('john', 2, true);
+console.log(friends);
