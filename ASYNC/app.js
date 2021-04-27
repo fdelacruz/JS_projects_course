b5fce452c91c3f3161f0e21a8a6dc539ced76ef5
Francisco De La Cruz <helloquico@gmail.com>
2021-04-27 18:40:02 -0400

    Reject Example

--- ASYNC/app.js
+++ ASYNC/app.js
@@ -2,26 +2,28 @@
 // Promises - Pending, Resolved, Rejected
 // then catch - pass another callback
 
-const heading1 = document.querySelector('.one')
-const heading2 = document.querySelector('.two')
-const heading3 = document.querySelector('.three')
-
+// const heading1 = document.querySelector('.one')
+// const heading2 = document.querySelector('.two')
+// const heading3 = document.querySelector('.three')
 const btn = document.querySelector('.btn')
+const container = document.querySelector('.img-container')
+const url = 'https://source.unsplash.com/random'
 
-btn.addEventListener('click', () => {})
-
-const promise = new Promise((resolve, reject) => {
-  const value = false
-  if (value) {
-    resolve([1, 2, 3])
-  } else {
-    reject('there was an error, value is false')
-  }
+btn.addEventListener('click', () => {
+  loadImage(url)
+    .then(data => container.appendChild(data))
+    .catch(err => console.log(err))
 })
-promise
-  .then(data => {
-    console.log(data)
+
+const loadImage = url => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', () => {
+      resolve(img)
+    })
+    img.addEventListener('error', () => {
+      reject(new Error(`Failed to load image from the source: ${url}`))
+    })
+    img.src = url
   })
-  .catch(err => {
-    console.log(err)
-  })
+}
