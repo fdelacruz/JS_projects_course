0c5637f8ae294ac2964bc4da18d6b958aec46a96
Francisco De La Cruz <helloquico@gmail.com>
2021-04-26 22:01:04 -0400

    Callback Hell

--- ASYNC/app.js
+++ ASYNC/app.js
@@ -5,13 +5,23 @@
 // Chop Onion
 // Add Onion, boil for 5 min
 // BROWSER!!! Fetch Data, Get Geolocation, setTimeout, setTimer, etc.
+// callbacks, promises, async/await
 
 const boilWater = time => {
   console.log('boiling...')
   setTimeout(() => {
     console.log('done.')
-  }, time)
+    console.log('add carrots')
+    setTimeout(() => {
+      console.log('carrots done')
+      console.log('add onions')
+      setTimeout(() => {
+        console.log('onion done')
+      }, 500)
+    }, 5000)
+    console.log('chop onion')
+  }, 10000)
 }
 
-boilWater(10000)
+boilWater()
 console.log('chop carrots')
