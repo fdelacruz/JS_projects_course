bab5b9c8ffc42aa206b7d4bce07656ca858becea
Francisco De La Cruz <helloquico@gmail.com>
2021-04-26 21:05:29 -0400

    Asynchronous

--- ASYNC/app.js
+++ ASYNC/app.js
@@ -4,17 +4,14 @@
 // Add Carrots, boil for 5 min
 // Chop Onion
 // Add Onion, boil for 5 min
+// BROWSER!!! Fetch Data, Get Geolocation, setTimeout, setTimer, etc.
 
 const boilWater = time => {
   console.log('boiling...')
-  for (let i = 0; i < time; i++) {
-    console.log('still not done...')
-  }
-  console.log('done.')
+  setTimeout(() => {
+    console.log('done.')
+  }, time)
 }
 
 boilWater(10000)
-console.log('chop onion')
-boilWater(5000)
-console.log('chop carrot')
-boilWater(5000)
+console.log('chop carrots')
