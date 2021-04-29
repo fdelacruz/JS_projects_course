262564b7c45a9cc91257e66f65effe92c3edf9fd
Francisco De La Cruz <helloquico@gmail.com>
2021-04-29 07:21:07 -0400

    JSON

--- AJAX/app.js
+++ AJAX/app.js
@@ -1,18 +1,27 @@
+// JSON - Javascript Object Notation
 const btn = document.querySelector('.btn')
+const url = './api/people.json';
+
 btn.addEventListener('click', () => {
-  getData()
+  getData(url)
 })
 
-const getData = () => {
+const getData = (url) => {
   const xhr = new XMLHttpRequest()
   console.log(xhr)
 
-  xhr.open('GET', './api/sample.txt')
+  xhr.open('GET', url)
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      const text = document.createElement('p')
-      text.textContent = xhr.responseText
-      document.body.appendChild(text)
+      // parse
+      // stringify
+      const data = JSON.parse(xhr.responseText);
+      const displayData = data.map((item) => {
+        return `<p>${item.name}</p>`
+      }).join('')
+      const element = document.createElement('div');
+      element.innerHTML = displayData
+      document.body.appendChild(element)
     } else {
       console.log({
         status: xhr.status,
