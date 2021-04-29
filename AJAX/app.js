8707ef2816affa2860e9659fa14b385e01445d3d
Francisco De La Cruz <helloquico@gmail.com>
2021-04-29 06:49:32 -0400

    Add a button

--- AJAX/app.js
+++ AJAX/app.js
@@ -1,18 +1,25 @@
-const xhr = new XMLHttpRequest()
-console.log(xhr)
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', () => {
+  getData()
+})
 
-xhr.open('GET', './api/sample.txt')
-xhr.onreadystatechange = () => {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    const text = document.createElement('p');
-    text.textContent = xhr.responseText;
-    document.body.appendChild(text)
-  } else {
-    console.log({
-      status: xhr.status,
-      text: xhr.statusText,
-      state: xhr.readyState
-    })
+const getData = () => {
+  const xhr = new XMLHttpRequest()
+  console.log(xhr)
+
+  xhr.open('GET', './api/sample.txt')
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const text = document.createElement('p')
+      text.textContent = xhr.responseText
+      document.body.appendChild(text)
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+        state: xhr.readyState
+      })
+    }
   }
+  xhr.send()
 }
-xhr.send()
