5a4be2b373f7895a45f11263b83e2f26df71ea73
Francisco De La Cruz <helloquico@gmail.com>
2021-04-28 23:31:14 -0400

    Simple Text

---
+++ AJAX/app.js
@@ -0,0 +1,18 @@
+const xhr = new XMLHttpRequest()
+console.log(xhr)
+
+xhr.open('GET', './api/sample.txt')
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const text = document.createElement('p');
+    text.textContent = xhr.responseText;
+    document.body.appendChild(text)
+  } else {
+    console.log({
+      status: xhr.status,
+      text: xhr.statusText,
+      state: xhr.readyState
+    })
+  }
+}
+xhr.send()
