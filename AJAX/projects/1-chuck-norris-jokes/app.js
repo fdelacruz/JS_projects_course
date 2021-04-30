6333d1ae9de4ce4d9e00a4b09d0732d3acecbfd3
Francisco De La Cruz <helloquico@gmail.com>
2021-04-30 07:55:58 -0400

    Random Joke - Promisify

--- AJAX/projects/1-chuck-norris-jokes/app.js
+++ AJAX/projects/1-chuck-norris-jokes/app.js
@@ -5,27 +5,35 @@ const URL = 'https://api.chucknorris.io/jokes/random'
 
 btn.addEventListener('click', () => {
   getData(URL)
+    .then(response => displayData(response))
+    .catch(err => console.log(err))
 })
 
 const getData = url => {
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET', url)
-  xhr.send()
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) return
-    if (xhr.status === 200) {
-      img.classList.add('shake-img')
-      const { value: joke } = JSON.parse(xhr.responseText)
-      content.textContent = joke
-      const random = Math.random() * 1000
-      setTimeout(() => {
-        img.classList.remove('shake-img')
-      }, random)
-    } else {
-      console.log({
-        status: xhr.status,
-        text: xhr.statusText
-      })
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.send()
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return
+      if (xhr.status === 200) {
+        resolve(xhr.responseText)
+      } else {
+        reject({
+          status: xhr.status,
+          text: xhr.statusText
+        })
+      }
     }
-  }
+  })
+}
+
+const displayData = (data) => {
+  img.classList.add('shake-img')
+  const { value: joke } = JSON.parse(data)
+  content.textContent = joke
+  const random = Math.random() * 1000
+  setTimeout(() => {
+    img.classList.remove('shake-img')
+  }, random)
 }
