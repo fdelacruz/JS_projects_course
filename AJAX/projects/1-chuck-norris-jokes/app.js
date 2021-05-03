d80438ad3de85176deb3aeee6e032f2ca4bb43cd
Francisco De La Cruz <helloquico@gmail.com>
2021-05-03 07:47:10 -0400

    Random Joke - Fetch

--- AJAX/projects/1-chuck-norris-jokes/app.js
+++ AJAX/projects/1-chuck-norris-jokes/app.js
@@ -4,33 +4,15 @@ const img = document.querySelector('.container img')
 const URL = 'https://api.chucknorris.io/jokes/random'
 
 btn.addEventListener('click', () => {
-  getData(URL)
+  fetch(URL)
+    .then(data => data.json())
     .then(response => displayData(response))
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err))
 })
 
-const getData = url => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', url)
-    xhr.send()
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4) return
-      if (xhr.status === 200) {
-        resolve(xhr.responseText)
-      } else {
-        reject({
-          status: xhr.status,
-          text: xhr.statusText
-        })
-      }
-    }
-  })
-}
-
-const displayData = (data) => {
+const displayData = ({ value: joke }) => {
   img.classList.add('shake-img')
-  const { value: joke } = JSON.parse(data)
+  // const { value: joke } = data
   content.textContent = joke
   const random = Math.random() * 1000
   setTimeout(() => {
