e73327c8ac1f30a390df4fda7301179853102e98
Francisco De La Cruz <helloquico@gmail.com>
2021-05-08 23:50:44 -0400

    Cocktails - Display Drinks

--- AJAX/projects/3-cocktails/src/presentDrinks.js
+++ AJAX/projects/3-cocktails/src/presentDrinks.js
@@ -1,11 +1,12 @@
 import fetchDrinks from './fetchDrinks.js'
+import displayDrinks from './displayDrinks.js'
 
 const showDrink = async url => {
   // fetch drinks
   const data = await fetchDrinks(url)
-  console.log(data)
 
   // display drinks
+  const section = await displayDrinks(data)
 }
 
 export default showDrink
